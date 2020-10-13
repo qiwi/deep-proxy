05f4ec73f13da23866fab4f44173057f12a36496
Anton Golub <golub.anton@gmail.com>
2020-10-13 18:41:53 +0300

    feat: add basic description and impl draft

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -1,1 +1,32 @@
-export const foo = (): string => 'bar'
+
+type TProxyContext<T> = {
+  target: T,
+  trap: string
+}
+type TProxyHandler = <T>(proxyContext: TProxyContext<T>) => any
+
+export const OTHERWISE = Symbol('otherwise')
+
+// @ts-ignore
+export interface DeepProxy<T extends object> extends T {} // eslint-disable-line
+
+// eslint-disable-next-line
+export class DeepProxy<T extends object> {
+  constructor (target: T, handler: TProxyHandler) {
+    const proxy = new Proxy({}, {get(_: any, prop: string) {
+      const proxyContext = {
+        target,
+        trap: prop
+      }
+
+      const result = handler(proxyContext)
+
+      return result === OTHERWISE
+        ? undefined
+        : () => result
+
+    }})
+
+    return new Proxy(target, proxy) as DeepProxy<T>
+  }
+}
