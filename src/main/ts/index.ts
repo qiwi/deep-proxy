1a96c8d0050413c3830eea98d1bf567dcf75aec3
Anton Golub <golub.anton@gmail.com>
2020-10-15 14:17:56 +0300

    feat: add handler directives PROXY and DEFAULT

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -1,32 +1,81 @@
+// eslint-disable-next-line
+type TTarget = object & Record<any, any>
 
-type TProxyContext<T> = {
-  target: T,
-  trap: string
+type TTrapName = keyof typeof Reflect
+
+type TTrap = <T extends TTarget>(target: T, prop: keyof T, val: any, receiver: any) => any
+
+// https://github.com/microsoft/TypeScript/issues/24220
+type TTraps = {
+  [key in TTrapName]: TTrap
 }
-type TProxyHandler = <T>(proxyContext: TProxyContext<T>) => any
 
-export const OTHERWISE = Symbol('otherwise')
+type THandlerContext<T> = {
+  target: T,
+  trapName: TTrapName
+}
+type TProxyHandler = <T>(proxyContext: THandlerContext<T>) => any
+
+type TTrapContext = {
+  trapName: TTrapName
+  root: TTarget
+  handler: TProxyHandler
+  path: string[]
+}
+
+export const DEFAULT = Symbol('default')
+export const PROXY = Symbol('proxy')
 
 // @ts-ignore
-export interface DeepProxy<T extends object> extends T {} // eslint-disable-line
+export interface DeepProxy<T extends TTarget> extends T {} // eslint-disable-line
 
-// eslint-disable-next-line
-export class DeepProxy<T extends object> {
-  constructor (target: T, handler: TProxyHandler) {
-    const proxy = new Proxy({}, {get(_: any, prop: string) {
-      const proxyContext = {
-        target,
-        trap: prop
-      }
+const trapNames = Object.keys(Object.getOwnPropertyDescriptors(Reflect)) as Array<TTrapName>
 
-      const result = handler(proxyContext)
+const createHandlerContext = <T>(trapContext: TTrapContext, target: T, prop?: keyof T, val?: any, receiver?: any) => {
+  const {path, root, trapName} = trapContext
+  const args = [target, prop, val, receiver]
+  const key = ['get', 'has', 'set', 'defineProperty', 'deleteProperty', 'getOwnPropertyDescriptor'].includes(trapName) ? prop : undefined
+  const value = key === undefined ? undefined : target[key]
+  const newValue = trapName === 'set' ? val : undefined
 
-      return result === OTHERWISE
-        ? undefined
-        : () => result
+  return {
+    args,
+    trapName,
+    path,
+    root,
+    target,
+    value,
+    newValue,
+    key
+  }
+}
 
-    }})
+const trap = function <T extends TTarget>(this: TTrapContext , target: T, prop: keyof T, val: any, receiver: any) {
+  const {trapName, handler, root} = this
+  const handlerContext = createHandlerContext(this, target, prop, val, receiver)
+  const {value} = handlerContext
+  const result = handler(handlerContext)
 
-    return new Proxy(target, proxy) as DeepProxy<T>
+  if (result === PROXY && typeof value === 'object') {
+    return new DeepProxy(value, handler, [prop as string], root)
+  }
+
+  if (result === DEFAULT) {
+    // eslint-disable-next-line
+    return Reflect[trapName](target as TTarget & Function, prop, val, receiver)
+  }
+
+  return result
+}
+
+const createTraps = (root: TTarget, handler: TProxyHandler, path: string[]) =>
+  trapNames.reduce((traps, trapName ): TTraps => {
+    traps[trapName] = trap.bind({path, root, trapName, handler})
+    return traps
+  }, {} as TTraps) as ProxyHandler<TTarget>
+
+export class DeepProxy<T extends TTarget> {
+  constructor (target: T, handler: TProxyHandler, path: string[] = [], root: TTarget = target) {
+    return new Proxy(target, createTraps(root, handler, path)) as DeepProxy<T>
   }
 }
