ef4e9830a13a6d162293915ad47e8b6b483e96a5
Anton Golub <golub.anton@gmail.com>
2020-10-16 19:36:16 +0300

    fix(package): repack in umd

---
+++ src/test/js/index.js
@@ -0,0 +1,13 @@
+describe('js builds', () => {
+  it('DeepProxy is properly exported (es5)', () => {
+    expect(typeof require('../../../target/es5').DeepProxy).toBe('function') // eslint-disable-line
+  })
+
+  it('DeepProxy is properly exported (es6)', () => {
+    expect(typeof require('../../../target/es6').DeepProxy).toBe('function') // eslint-disable-line
+  })
+
+  it('DeepProxy is properly exported (bundle)', () => {
+    expect(typeof require('../../../target/bundle/deep-proxy').DeepProxy).toBe('function') // eslint-disable-line
+  })
+})
