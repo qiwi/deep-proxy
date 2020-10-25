ef44497d959336099a30af32f27d327b1f1c9dc6
Anton Golub <golub.anton@gmail.com>
2020-10-25 16:23:48 +0300

    fix: use WeakMap instead of Map for target refs

--- src/main/ts/cache.ts
+++ src/main/ts/cache.ts
@@ -8,8 +8,10 @@ const cache: TProxyCache = {
 const getCache = <M extends WeakMap<any, any>, K>(
   map: M,
   key: K,
-  Constructor: any = Map,
+  Constructor: any
 ) => map.get(key) || map.set(key, new Constructor()).get(key)
+
+const getKey = (path: string[]): string => path.join(​) // eslint-disable-line no-irregular-whitespace
 
 export const addToCache = <T extends TTarget>(
   root: TTarget,
@@ -18,7 +20,7 @@ export const addToCache = <T extends TTarget>(
   traps: TTraps,
   proxy: TProxy<T>,
 ): void => {
-  getCache(getCache(cache.traps, root), target, Map).set(path.join('.'), traps)
+  getCache(getCache(cache.traps, root, WeakMap), target, Map).set(getKey(path), traps)
   cache.proxies.set(traps, proxy)
 }
 
@@ -28,5 +30,5 @@ export const getFromCache = <T extends TTarget>(
   path: string[],
 ): TTarget | undefined =>
   cache.proxies.get(
-    cache.traps.get(root)?.get(target)?.get(path.join('.')) as TTraps,
+    cache.traps.get(root)?.get(target)?.get(getKey(path)) as TTraps,
   )
